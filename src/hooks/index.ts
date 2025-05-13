34c0dda522463cef03b74942c17127fd1449a4c1
Rajat Lekhari <rajat.astalavista@gmail.com>
2025-05-13 19:43:26 +0530

    changes

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,8 +1,9 @@
 
 // Re-export hooks
-export * from './use-branch';
-export * from './use-supabase-query';
-export * from './use-sms-settings';
-export * from './use-email-settings';
-export * from './use-whatsapp-settings';
-export * from './use-message-templates';
+// Export named exports from hooks
+export { useBranch } from './use-branch';
+export { useSupabaseQuery } from './use-supabase-query';
+export { useSmsSettings } from './use-sms-settings';
+export { useEmailSettings } from './use-email-settings';
+export { useWhatsAppSettings } from './use-whatsapp-settings';
+export { useMessageTemplates } from './use-message-templates';
