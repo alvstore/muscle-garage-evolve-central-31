85ddd94d3fd0f2d1dc58ae10c4064e9eee6292ce
Rajat Lekhari <rajat.astalavista@gmail.com>
2025-05-20 12:06:33 +0530

    Update member.ts

--- src/types/members/member.ts
+++ src/types/members/member.ts
@@ -30,9 +30,6 @@ export interface Member {
   blood_group?: string;
   id_type?: string;
   id_number?: string;
-  emergency_contact_name?: string;
-  emergency_contact_phone?: string;
-  emergency_contact_relation?: string;
   // For backward compatibility
   trainerId?: string;
   membershipStartDate?: string;
